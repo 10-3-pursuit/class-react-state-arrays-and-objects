85bbaf9aa78eefb97dfe549e23529bcf7e29a00e
jd richards <jdrichardstech@gmail.com>
2023-12-09 11:35:11 -0500

    update ids

--- src/data.js
+++ src/data.js
@@ -1,30 +1,30 @@
 export const dogsData = [
   {
-    id: 46,
+    id: "87142940-96b0-11ee-9f16-e1ccb67c3da6",
     name: "Mitsy",
     present: true,
     grade: "60",
-    notes: "Stole Prince's toys repeatedly.",
+    notes: "Stole Prince's toys repeatedly."
   },
   {
-    id: 79,
+    id: "8685a4e0-96b0-11ee-9f16-e1ccb67c3da6",
     name: "Misty",
     present: false,
     grade: "90",
-    notes: "We missed Misty today. We hope we see her tomorrow",
+    notes: "We missed Misty today. We hope we see her tomorrow"
   },
   {
-    id: 34,
+    id: "85575e10-96b0-11ee-9f16-e1ccb67c3da6",
     name: "Bingo",
     present: true,
     grade: "65",
-    notes: "Had 3 baths today. He kept rolling in the mud.",
+    notes: "Had 3 baths today. He kept rolling in the mud."
   },
   {
-    id: 65,
+    id: "81ad8ff0-96b0-11ee-9f16-e1ccb67c3da6",
     name: "Prince",
     present: true,
     grade: "80",
-    notes: "It was his first day and he hid under a chair for most of it.",
-  },
+    notes: "It was his first day and he hid under a chair for most of it."
+  }
 ];
